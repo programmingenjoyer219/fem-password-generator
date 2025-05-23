1daa7f9dadb12cc288412646bb7396bb9bdf74cc
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-23 19:06:14 +0530

    Provide password as a prop.
    - PasswordDisplay is passed the password state in the passwordGenerator as a prop.
    - This is done to display the newly generated password and to make it available to be copied to clipboard.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
+	import { passwordGenerator } from '$lib/password.svelte';
 	import PasswordConfig from '$lib/PasswordConfig.svelte';
 	import PasswordDisplay from '$lib/PasswordDisplay.svelte';
 </script>
 
 <h1 class="text-md text-light-400 text-center font-medium">Password Generator</h1>
-<PasswordDisplay />
+<PasswordDisplay password={passwordGenerator.password} />
 <PasswordConfig />
