73793b785004d531963d1bcba15c8767a1aed7cd
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-24 20:16:13 +0530

    Create password generator function.
    - A function named generatePassword is created along with helper functions to aid in password generation.

--- src/lib/password.svelte.ts
+++ src/lib/password.svelte.ts
@@ -1,3 +1,9 @@
+import { pickRandomItem } from './utils';
+
+type OptionKey = 'uppercaseLetters' | 'lowercaseLetters' | 'numbers' | 'symbols';
+type Options = Record<OptionKey, boolean>;
+export type PasswordConfig = { passwordLength: number } & Options;
+
 let password = $state('P4$SW0r|)!');
 
 export const passwordStore = {
@@ -8,3 +14,45 @@ export const passwordStore = {
 		password = v;
 	}
 };
+
+const INGREDIENTS: Record<OptionKey, string[]> = {
+	uppercaseLetters: [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+	lowercaseLetters: [...'abcdefghijklmnopqrstuvwxyz'],
+	numbers: [...'0123456789'],
+	symbols: [...'!@#$%^&*()_+[]{}|;:,.<>?/`~-=']
+};
+
+export function generatePassword(config: PasswordConfig): [string | null, string | null] {
+	const { passwordLength, ...options } = config;
+
+	if (passwordLength == 0) {
+		return [null, 'password length cannot be zero'];
+	}
+
+	if (noOptionsSelected(config)) {
+		return [null, 'please select atleast one option'];
+	}
+
+	const selectedOptions = Object.keys(options).filter(function filterInSelectedOptions(option) {
+		return options[option as OptionKey];
+	}) as OptionKey[];
+
+	let result = '';
+
+	while (result.length != passwordLength) {
+		result += getRandomChar(selectedOptions);
+	}
+
+	return [result, null];
+}
+
+export function noOptionsSelected(config: PasswordConfig): boolean {
+	const { lowercaseLetters, uppercaseLetters, numbers, symbols } = config;
+	return !(lowercaseLetters || uppercaseLetters || numbers || symbols);
+}
+
+function getRandomChar(selectedOptions: OptionKey[]): string {
+	const randomOption = pickRandomItem(selectedOptions);
+	const randomChar = pickRandomItem(INGREDIENTS[randomOption]);
+	return randomChar;
+}
