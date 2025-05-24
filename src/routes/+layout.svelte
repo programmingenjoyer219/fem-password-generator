69d490af75c5cf52b55b887bfb57397f290a16ce
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-24 21:37:57 +0530

    Set title.
    - Set app title to Password Generator in svelte:head.
    - This is done to display the application's name in the browser tabs section.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,8 @@
 		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
 		rel="stylesheet"
 	/>
+
+	<title>Password Generator</title>
 </svelte:head>
 
 <main class="container mx-auto max-w-md space-y-4 px-6 py-8">
