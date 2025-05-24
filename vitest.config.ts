a21d29ddcabb3cfec5fb53756a98676b6ce2cf8f
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-24 11:40:51 +0530

    Add svelte plugin to vitest config.
    - Add svelte plugin to vitest.config.ts.
    - This is done to support testing for code (making use of svelte 5 runes) inside '.svelte.test.ts' files.
    - This now allows testing of PasswordGenerator class.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	test: {
-		globals: true
-	}
+		globals: true,
+		include: ['**/*.test.ts', '**/*.test.js', '**/*.svelte.test.ts'], // Include Svelte tests
+		environment: 'jsdom'
+	},
+	plugins: [svelte()]
 });
