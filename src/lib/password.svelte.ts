b114ac286b161d4013db34e990d8c5602882bedf
programmingenjoyer219 <yuganshu219@gmail.com>
2025-05-24 14:41:27 +0530

    Make PasswordGenerator functional.
    - generatePassword, and evaluateStrength methods are now functional. Additional methods have been added. reset method is updated set boolean states to true and not set password to empty string.
    - This is done to make password generation functionality work.

--- src/lib/password.svelte.ts
+++ src/lib/password.svelte.ts
@@ -1,4 +1,8 @@
+import { pickRandomItem } from './utils';
+
 export type PasswordStrengthLevel = 'weak' | 'medium' | 'strong';
+type OptionKey = 'uppercase' | 'lowercase' | 'numbers' | 'symbols';
+type Options = Record<OptionKey, boolean>;
 
 export class PasswordGenerator {
 	static INGREDIENTS = {
@@ -67,29 +71,82 @@ export class PasswordGenerator {
 	};
 	// --------
 	length: number = $state(10);
-	uppercase: boolean = $state(false);
-	lowercase: boolean = $state(false);
-	numbers: boolean = $state(false);
-	symbols: boolean = $state(false);
+	uppercase: boolean = $state(true);
+	lowercase: boolean = $state(true);
+	numbers: boolean = $state(true);
+	symbols: boolean = $state(true);
 	// --------
 	password: string = $derived.by(this.generatePassword.bind(this));
 	strength: PasswordStrengthLevel = $derived.by(this.evaluateStrength.bind(this));
 
 	generatePassword(): string {
-		return 'P4$SW0rD!';
+		if (this.length < 10) throw new Error('length should be atleast 10');
+
+		if (this.noOptionsSelected())
+			throw new Error('all boolean states are false, atleast one should be true');
+
+		let result = '';
+
+		while (result.length != this.length) {
+			result += this.getRandomChar();
+		}
+
+		return result;
+	}
+
+	noOptionsSelected(): boolean {
+		return !(this.lowercase || this.uppercase || this.numbers || this.symbols);
+	}
+
+	getRandomChar(): string {
+		const selectedOptions = this.getSelectedOptions();
+
+		const randomOption = pickRandomItem(selectedOptions);
+
+		if (randomOption == 'uppercase') {
+			return pickRandomItem(PasswordGenerator.INGREDIENTS.uppercaseLetters);
+		} else if (randomOption == 'lowercase') {
+			return pickRandomItem(PasswordGenerator.INGREDIENTS.uppercaseLetters).toLowerCase();
+		} else if (randomOption == 'numbers') {
+			return Math.floor(Math.random() * 10) + '';
+		} else {
+			return pickRandomItem(PasswordGenerator.INGREDIENTS.symbols);
+		}
 	}
 
 	evaluateStrength(): PasswordStrengthLevel {
-		return 'weak';
+		const selectedOptions = this.getSelectedOptions();
+
+		if (selectedOptions.length >= 3) {
+			return 'strong';
+		} else if (selectedOptions.length >= 2) {
+			return 'medium';
+		} else {
+			return 'weak';
+		}
+	}
+
+	getSelectedOptions(): OptionKey[] {
+		const options: Options = {
+			uppercase: this.uppercase,
+			lowercase: this.lowercase,
+			numbers: this.numbers,
+			symbols: this.symbols
+		};
+
+		const selectedOptions = Object.keys(options).filter(function filterInSelectedOptions(option) {
+			return options[option as OptionKey];
+		}) as OptionKey[];
+
+		return selectedOptions;
 	}
 
 	reset() {
-		this.password = '';
 		this.length = 10;
-		this.uppercase = false;
-		this.lowercase = false;
-		this.lowercase = false;
-		this.symbols = false;
+		this.uppercase = true;
+		this.lowercase = true;
+		this.lowercase = true;
+		this.symbols = true;
 	}
 }
 
